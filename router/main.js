8653999ac520ca8fa27cd4c64a6c17a03d91eb73
nyaco0125 <catpacross@gmail.com>
2021-10-12 20:27:58 +0900

    'backwards'

--- router/main.js
+++ router/main.js
@@ -1,15 +1,42 @@
 import express from 'express';
-import * as controller from '../controller/main.js';
 
 const router = express.Router();
 
+let anouncements = []
 
-router.get('/', controller.get);
+router.get('/', (req, res, next) => {
+    res.status(200).json(anouncements);
+})
 
-router.post('/admin', controller.post);
 
-router.put('/admin/:id', controller.put);
+router.post('/admin', (req, res, next) => {
+    const data = {
+        id : Date.now(),
+        title : req.body.title,
+        content : req.body.content
+    }
+    anouncements = [data, ...anouncements];
+    res.status(201).json(anouncements);
+});
 
-router.delete('/admin/:id', controller.remove)
+router.put('/admin/:id', (req, res, next) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    const content = req.body.content;
+    const anouncement = anouncements.find((one)=>one.id == id);
+    if(anouncement){
+        anouncement.title = title;
+        anouncement.content = content;
+        res.status(200).json(anouncements);
+    } else {
+        res.status(404);
+    }
+});
+
+router.delete('/admin/:id', (req, res, next) => {
+    const id = req.params.id;
+    anouncements = anouncements.filter((t) => t.id != id);
+    res.sendStatus(204);
+})
 
 export default router;
